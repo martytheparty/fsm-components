94cd6a258e13876698a8a201ef69968e3f6008ac
Marty <marty@ilikeemail.com>
2020-08-13 20:48:29 -0400

    Sets up a component.

---
+++ girl-login/girl-login/src/GirlLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './girl-login.jpg';
+
+function GirlLogin() {
+  return (
+    <div className="App">
+      <img src={logo} alt="login" />
+    </div>
+  );
+}
+
+export default GirlLogin;
--- girl-login/girl-login/src/index.js
+++ girl-login/girl-login/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GirlLogin from './GirlLogin'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GirlLogin />
   </React.StrictMode>,
   document.getElementById('root')
 );
